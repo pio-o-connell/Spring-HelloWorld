1e6e99e7462da92c3f04a33e87006c6cab62971c
pio-o-connell <pio_o_con@hotmail.com>
2016-04-20 17:26:45 +0100

    Inital Config

---
+++ LocalHelloWorld/HelloWorld/src/main/java/com/HDipCloud/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.HDipCloud;
+
+public class HelloWorld {
+	
+	private String message;
+	
+	public void setMessage(String helloMessage) {
+		this.message = helloMessage;
+	}
+	
+	public void sayHello() {
+		System.out.println(message);
+	}
+	
+}
